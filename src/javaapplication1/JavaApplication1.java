6ab92d3b9aa05a39c12208bcc499128e88812b7a
NadaML <nada.mlotfy@gmail.com>
2017-03-24 23:37:09 +0200

    n

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,9 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hiiii");
+        System.out.println("hiiii all");
+     
+        
     }
     
 }
